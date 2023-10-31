3d541bef67266cf23ca21ed749259c804361bf38
Wallys Ferreira <wallysfsilva@gmail.com>
2023-10-31 14:34:44 -0300

    create function to create chart

--- front/dashboard/src/app/components/chart/chart.component.ts
+++ front/dashboard/src/app/components/chart/chart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-chart',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./chart.component.css']
 })
 export class ChartComponent {
+  public chart: any
 
+  createChart(): void {
+    new Chart('chartDiv', {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue'],
+        datasets: [{
+          label: '# of votes',
+          data: [12, 19],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    })
+  }
 }
