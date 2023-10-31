43add195c8b305341999260d4481f0c3353c8551
Wallys Ferreira <wallysfsilva@gmail.com>
2023-10-31 15:22:46 -0300

    change icon depending on active route

--- front/dashboard/src/app/components/menu/menu.component.ts
+++ front/dashboard/src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-
+  constructor(public router: Router){
+  }
 }
