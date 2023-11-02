07c59c80de49cec2990d60e4ed23b1b9942d36f8
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-02 13:30:31 -0300

    implement finding user by id

--- back/service/user.go
+++ back/service/user.go
@@ -1,5 +1,12 @@
 package service
 
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
 type User struct {
 	id      int
 	name    string
@@ -7,5 +14,22 @@ type User struct {
 }
 
 func (rp *RepositorioPostgre) findUser(id int) User {
-	return User{}
+	user := User{}
+
+	db, err := sql.Open("postgres", rp.connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT id, name, segment FROM users WHERE id = $1", id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		rows.Scan(&user.id, &user.name, &user.segment)
+	}
+
+	return user
 }
