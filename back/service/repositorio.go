a478a27d3a3dbf84727be278e39b21329dadf11e
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-03 11:03:58 -0300

    receive dates as time

--- back/service/repositorio.go
+++ back/service/repositorio.go
@@ -1,10 +1,12 @@
 package service
 
+import "time"
+
 type Repositorio interface {
 	findUser(id int) (User, error)
 	findUsers(segment string) []User
 	findExtract(id int) (Extract, error)
-	findExtracts(created_at string, pages_processed int, doc_type string, user_id int) []Extract
+	findExtracts(date_start time.Time, date_end time.Time, created_at string, pages_processed int, doc_type string, user_id int) []Extract
 }
 
 type RepositorioPostgre struct {
