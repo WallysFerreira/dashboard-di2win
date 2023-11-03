5cd25b462aa72c7409892f291bdf8bfa7cf47515
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-03 10:03:04 -0300

    add new method to interface

--- back/service/repositorio.go
+++ back/service/repositorio.go
@@ -4,6 +4,7 @@ type Repositorio interface {
 	findUser(id int) (User, error)
 	findUsers(segment string) []User
 	findExtract(id int) (Extract, error)
+	findExtracts(created_at string, pages_processed int, doc_type string, user_id int) []Extract
 }
 
 type RepositorioPostgre struct {
