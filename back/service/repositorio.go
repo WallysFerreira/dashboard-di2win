2dd830a44c2ac91a467408d3615b51bc26d78306
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-03 22:28:28 +0000

    make RepositorioPostgre implement Repositorio

--- back/service/repositorio.go
+++ back/service/repositorio.go
@@ -6,7 +6,7 @@ type Repositorio interface {
 	findUser(id int) (User, error)
 	findUsers(segment string) []User
 	findExtract(id int) (Extract, error)
-	findExtracts(date_start time.Time, date_end time.Time, created_at string, pages_processed int, doc_type string, user_id int) []Extract
+	findExtracts(date_start time.Time, date_end time.Time, pages_processed int, doc_type string, user_id int) []Extract
 }
 
 type RepositorioPostgre struct {
