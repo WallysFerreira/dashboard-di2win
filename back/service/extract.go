9c9ad66bb58eb3a580673697acfdff81dc60b8ba
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-03 09:29:12 -0300

    implement findExtract

--- back/service/extract.go
+++ back/service/extract.go
@@ -1,6 +1,10 @@
 package service
 
-import "time"
+import (
+	"database/sql"
+	"log"
+	"time"
+)
 
 type Extract struct {
 	id              int
@@ -8,4 +12,31 @@ type Extract struct {
 	pages_processed int
 	doc_type        string
 	user_id         int
+}
+
+func (rp *RepositorioPostgre) findExtract(id int) (Extract, error) {
+	found := Extract{}
+	var unparsed_date string
+
+	db, err := sql.Open("postgres", rp.connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM extracts WHERE id = $1", id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		rows.Scan(&found.id, &unparsed_date, &found.pages_processed, &found.doc_type, &found.user_id)
+	}
+
+	found.created_at, err = time.Parse(time.RFC3339, unparsed_date)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return found, nil
 }
