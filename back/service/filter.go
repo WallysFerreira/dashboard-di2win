7594502394a440f306dc6afe2339f24d72d6907a
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-04 10:05:13 -0300

    implement gerarFiltro for dates

--- back/service/filter.go
+++ back/service/filter.go
@@ -12,7 +12,7 @@ type Filtro interface {
 
 type FiltroExtract struct {
 	DataStart      time.Time
-	DateEnd        time.Time
+	DataEnd        time.Time
 	PagesProcessed int
 	DocType        string
 	UserId         int
@@ -20,6 +20,7 @@ type FiltroExtract struct {
 
 func (fe FiltroExtract) gerarFiltro() string {
 	filter := ""
+	filtered := false
 	filter_count := 0
 	fe_reflected := reflect.ValueOf(fe)
 	fe_length := fe_reflected.NumField()
@@ -29,15 +30,25 @@ func (fe FiltroExtract) gerarFiltro() string {
 		field_name := fe_reflected.Type().Field(i).Name
 		field_type := field.Type()
 
-		if filter_count > 0 {
-			filter = fmt.Sprintf("%s AND", filter)
-		}
-
 		if field_type.Name() == "Time" && !field.IsZero() {
+			filter = checarContagem(filter_count, filter)
+
+			switch field_name {
+			case "DataStart":
+				year, month, day := fe.DataStart.Date()
+				filter = fmt.Sprintf("%s created_at > '%d-%d-%d'", filter, year, month, day)
+			case "DataEnd":
+				year, month, day := fe.DataEnd.Date()
+				filter = fmt.Sprintf("%s created_at < '%d-%d-%d'", filter, year, month, day)
+			}
+
 			filter_count++
+			filtered = true
 		}
 
 		if field_type.Name() == "int" && field.Int() > 0 {
+			filter = checarContagem(filter_count, filter)
+
 			switch field_name {
 			case "PagesProcessed":
 				filter = fmt.Sprintf("%s pages_process = %d", filter, field.Int())
@@ -46,16 +57,28 @@ func (fe FiltroExtract) gerarFiltro() string {
 			}
 
 			filter_count++
+			filtered = true
 		}
 
 		if field_type.Name() == "string" && field.String() != "" {
+			filter = checarContagem(filter_count, filter)
+
 			filter_count++
+			filtered = true
 		}
 	}
 
-	if filter_count > 0 {
+	if filtered {
 		filter = fmt.Sprintf("WHERE%s", filter)
 	}
 
 	return filter
+}
+
+func checarContagem(count int, filter string) string {
+	if count > 0 {
+		return fmt.Sprintf("%s AND", filter)
+	}
+
+	return ""
 }
--- back/service/service_test.go
+++ back/service/service_test.go
@@ -226,4 +226,17 @@ func TestFilter(t *testing.T) {
 			t.Errorf("Expected '%s', got '%s'", expected, got)
 		}
 	})
+
+	t.Run("generate filter with one date", func(t *testing.T) {
+		filtro := FiltroExtract{}
+		filtro.DataEnd = time.Date(2022, 9, 22, 0, 0, 0, 0, time.UTC)
+
+		expected := "WHERE created_at < '2022-9-22'"
+
+		got := filtro.gerarFiltro()
+
+		if expected != got {
+			t.Errorf("Expected '%s', got '%s'", expected, got)
+		}
+	})
 }
