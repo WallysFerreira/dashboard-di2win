ca0103f0664dc7ab7f33161b809c46cabf1f0129
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-04 10:02:22 -0300

    write new test

--- back/service/service_test.go
+++ back/service/service_test.go
@@ -200,12 +200,25 @@ func TestExtracts(t *testing.T) {
 }
 
 func TestFilter(t *testing.T) {
-	filtro := FiltroExtract{}
-
-	t.Run("generate filter with 1 option", func(t *testing.T) {
+	t.Run("generate filter with int", func(t *testing.T) {
+		filtro := FiltroExtract{}
 		filtro.UserId = 8
 
 		expected := "WHERE user_id = 8"
+
+		got := filtro.gerarFiltro()
+
+		if expected != got {
+			t.Errorf("Expected '%s', got '%s'", expected, got)
+		}
+	})
+
+	t.Run("generate filter with date", func(t *testing.T) {
+		filtro := FiltroExtract{}
+		filtro.DataStart = time.Date(2023, 10, 1, 0, 0, 0, 0, time.UTC)
+		filtro.DataEnd = time.Date(2023, 10, 23, 0, 0, 0, 0, time.UTC)
+
+		expected := "WHERE created_at > '2023-10-1' AND created_at < '2023-10-23'"
 
 		got := filtro.gerarFiltro()
 
