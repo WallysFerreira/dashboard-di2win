0623638d9e944af2d36c6f537410466890566bb9
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-04 09:39:54 -0300

    implement gerarFiltro for int type

--- back/service/filter.go
+++ back/service/filter.go
@@ -26,20 +26,35 @@ func (fe FiltroExtract) gerarFiltro() string {
 
 	for i := 0; i < fe_length; i++ {
 		field := fe_reflected.Field(i)
+		field_name := fe_reflected.Type().Field(i).Name
 		field_type := field.Type()
 
 		if filter_count > 0 {
-			filter = fmt.Sprintf("%s AND ", filter)
+			filter = fmt.Sprintf("%s AND", filter)
 		}
 
 		if field_type.Name() == "Time" && !field.IsZero() {
+			filter_count++
 		}
 
 		if field_type.Name() == "int" && field.Int() > 0 {
+			switch field_name {
+			case "PagesProcessed":
+				filter = fmt.Sprintf("%s pages_process = %d", filter, field.Int())
+			case "UserId":
+				filter = fmt.Sprintf("%s user_id = %d", filter, field.Int())
+			}
+
+			filter_count++
 		}
 
 		if field_type.Name() == "string" && field.String() != "" {
+			filter_count++
 		}
+	}
+
+	if filter_count > 0 {
+		filter = fmt.Sprintf("WHERE%s", filter)
 	}
 
 	return filter
