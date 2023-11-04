548362b1a28f7f2985aa34eea1bf940ede3ae830
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-04 08:53:39 -0300

    create struct FiltroExtract

--- back/service/filter.go
+++ back/service/filter.go
@@ -1,5 +1,15 @@
 package service
 
+import "time"
+
 type Filtro interface {
 	gerarFiltro() string
+}
+
+type FiltroExtract struct {
+	DataStart      time.Time
+	DateEnd        time.Time
+	PagesProcessed int
+	DocType        string
+	UserId         int
 }
