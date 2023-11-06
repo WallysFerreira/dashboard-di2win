129f3f43e00e4a3f7bdbb547544b80eb6afa0e2d
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-06 18:47:06 -0300

    make FindExtracts return only the extracts

--- back/service/repositorio.go
+++ back/service/repositorio.go
@@ -6,7 +6,7 @@ type Repositorio interface {
 	FindUser(id int) (User, error)
 	FindUsers(segment string) []User
 	FindExtract(id int) (Extract, error)
-	FindExtracts(filter Filtro) ([]Extract, int)
+	FindExtracts(filter Filtro) []Extract
 	CountExtracts(group_by string, filter Filtro) []model.Count
 }
 
