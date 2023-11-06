a68dd3ca7ae30db62e8d0c8f6683e96c73ccb486
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-06 17:56:55 -0300

    write new test

--- back/service/count_test.go
+++ back/service/count_test.go
@@ -42,4 +42,31 @@ func TestCount(t *testing.T) {
 			t.Errorf("Expected %v, got %v", expected, got)
 		}
 	})
+
+	t.Run("count where user_id is 5 grouping by document", func(t *testing.T) {
+		group_by := "doc_type"
+		filter := FiltroExtract{
+			UserId: 5,
+		}
+
+		expected := []model.Count{
+			{Name: "FATURAMENTO", Value: 52},
+			{Name: "CNH", Value: 62},
+			{Name: "POSICAO_CONSOLIDADA", Value: 138},
+			{Name: "FATURA_ENERGIA", Value: 43},
+			{Name: "CONTRATO_SOCIAL", Value: 47},
+			{Name: "CAPA_SERASA", Value: 90},
+			{Name: "DECLARACAO_IR", Value: 158},
+			{Name: "ENDIVIDAMENTO", Value: 139},
+			{Name: "BALANCO_PATRIMONIAL", Value: 174},
+			{Name: "COMPROVANTE_RESIDENCIA", Value: 49},
+			{Name: "RECIBOS", Value: 35},
+		}
+
+		got := rep.CountExtracts(group_by, filter)
+
+		if !reflect.DeepEqual(expected, got) {
+			t.Errorf("Expected %v, got %v", expected, got)
+		}
+	})
 }
