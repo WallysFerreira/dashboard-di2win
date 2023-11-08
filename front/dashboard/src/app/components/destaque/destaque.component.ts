351fa16a7dff2783c5351d184289228570c87987
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-08 15:43:26 -0300

    receive component parameters

--- front/dashboard/src/app/components/destaque/destaque.component.ts
+++ front/dashboard/src/app/components/destaque/destaque.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-destaque',
   templateUrl: './destaque.component.html',
   styleUrls: ['./destaque.component.css']
 })
-export class DestaqueComponent {
 
+
+export class DestaqueComponent {
+  @Input() titulo!: string 
+  @Input() destaque!: string
 }
