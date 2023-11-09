3d1c88db0d6f569f058cf7c391ef4faeac889541
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-09 14:52:46 -0300

    get filter information

--- front/dashboard/src/app/pages/grafico/grafico.component.ts
+++ front/dashboard/src/app/pages/grafico/grafico.component.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
 })
 
 export class GraficoComponent {
-  
+  filtroButtons: any
+
+  ngAfterContentChecked() {
+    this.filtroButtons = document.getElementById('filtroDiv')?.getElementsByTagName('button')
+    
+    for (let button of this.filtroButtons) {
+      let idPai = button.parentElement.parentElement.id
+      if (button.classList == 'selected') {
+        if (idPai == 'docDiv') {
+          console.log('Documento selecionado:', button.value)
+        } else if (idPai == 'userDiv') {
+          console.log('Empresa selecionada:', button.value)
+        }
+      }
+    }
+  }
 }
