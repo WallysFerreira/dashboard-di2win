ee0fdc1489b5c0c0a5406f115dd4d0bcc0e63f78
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-10 15:04:18 -0300

    receive label and value data and update chart

--- front/dashboard/src/app/components/chart/chart.component.ts
+++ front/dashboard/src/app/components/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
 
 @Component({
@@ -10,22 +10,26 @@ import { Chart, registerables } from 'chart.js';
 export class ChartComponent {
   public chart: any
   buttonsGroup: any
+  @Input() label: any
+  @Input() data: any
+  setLabel: any = []
+  setData: any = []
 
   createChart(): void {
     this.chart = new Chart('chartCanvas', {
       type: 'bar',
       data: {
-        labels: ['Red', 'Blue'],
+        labels: this.setLabel,
         datasets: [{
           label: '# of votes',
-          data: [12, 19],
+          data: this.setData,
           borderWidth: 1
         }]
       },
       options: {
         scales: {
           y: {
-            beginAtZero: true
+            beginAtZero: true,
           }
         }
       }
@@ -38,7 +42,16 @@ export class ChartComponent {
 
   ngAfterContentChecked() {
     this.buttonsGroup = document.getElementById('groupDiv')?.getElementsByTagName('button')
+
+    if (JSON.stringify(this.data) !== JSON.stringify(this.setData)) {
+      this.setLabel = this.label
+      this.setData = this.data
+
+      this.chart.destroy()
+      this.createChart()
+    }
   }
+
 
   setSelected(id: any) {
     this.clearSelectedButton()
