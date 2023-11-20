7fc2a19a5a3bb7dac11675675dc39c80fec149eb
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-20 19:11:05 -0300

    use canvas context to create charts

--- front/dashboard/src/app/components/main-chart/main-chart.component.ts
+++ front/dashboard/src/app/components/main-chart/main-chart.component.ts
@@ -10,6 +10,10 @@ import { Chart, registerables } from 'chart.js';
 export class MainChartComponent {
   public chart: any
   buttonsGroup: any
+  canvasID: any
+  ctx: any
+  @Input() divWidth: any
+  @Input() chartType: any = 'bar'
   @Input() data: any
   @Input() groupingButtons: any
   setData: any = {
@@ -21,30 +25,36 @@ export class MainChartComponent {
   }
 
   createChart(): void {
-    this.chart = new Chart('chartCanvas', {
-      type: 'bar',
-      data: this.setData,
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
+    let canvas = <HTMLCanvasElement> document.getElementById(this.canvasID)
+    this.ctx = canvas?.getContext('2d')
+
+    if (this.ctx) {
+      this.chart = new Chart(this.ctx, {
+        type: this.chartType,
+        data: this.setData,
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            }
           }
         }
-      }
-    })
+      })
+    }
   }
 
   ngOnInit(): void {
-    this.createChart()
+    this.canvasID = 'chartCanvas' + Math.floor(Math.random() * 3)
   }
 
   ngAfterContentChecked() {
     this.buttonsGroup = document.getElementById('groupDiv')?.getElementsByTagName('button')
+    console.log(this.ctx)
 
     if (this.data !== this.setData) {
       this.setData = this.data
 
-      this.chart.destroy()
+      if (this.chart) this.chart.destroy()
       this.createChart()
     }
   }
