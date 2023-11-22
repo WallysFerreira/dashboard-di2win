c8b8009e5ea644215c9cdae4ecb154de87b8d700
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-22 14:18:21 -0300

    fetch segments that had the biggest amount of pages processed

--- front/dashboard/src/app/pages/home/home.component.ts
+++ front/dashboard/src/app/pages/home/home.component.ts
@@ -26,7 +26,8 @@ export class HomeComponent {
   empresaMaisTestouSemana!: string
   empresaMenosTestouMes!: string
   empresaMenosTestouSemana!: string
-  segmentoMaisTestou!: string
+  segmentoMaisTestouMes!: string
+  segmentoMaisTestouSemana!: string
   qntTotalPaginasTestadas: number = 0
   userChartData: any
   docChartData: any
@@ -74,6 +75,9 @@ export class HomeComponent {
     res = await getCount("users.name", false, "0", null, mesPassadoString, hojeString).then((res) => res.data.count)
     this.empresaMaisTestouMes = res.length != 0 ? res[0].name : "Nenhum"
     this.empresaMenosTestouMes = res.length != 0 ? res[res.length - 1].name : "Nenhum"
+
+    res = await getCount("users.segment", false, "0", null, null, null).then((res) => res.data.count)
+    this.segmentoMaisTestouMes = res.length != 0 ? res[0].name : "Nenhum"
   }
 
   async pegarInfoSemana() {
@@ -87,6 +91,9 @@ export class HomeComponent {
     res = await getCount("users.name", false, "0", null, semanaPassadaString, hojeString).then((res) => res.data.count)
     this.empresaMaisTestouSemana = res.length != 0 ? res[0].name : "Nenhum"
     this.empresaMenosTestouSemana = res.length != 0 ? res[res.length - 1].name : "Nenhum"
+
+    res = await getCount("users.segment", false, "0", null, null, null).then((res) => res.data.count)
+    this.segmentoMaisTestouSemana = res.length != 0 ? res[0].name : "Nenhum"
   }
 
   async pegarDadosDocumentos() {
